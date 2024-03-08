51a091bdcdca60f4555a13bff9b83eff37a5880e
Rajdip <108176929+Rajdip585@users.noreply.github.com>
2024-03-08 18:20:39 +0530

    update js/script.js

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent default form submission
+  var webhookUrl = "https://api.bots.business/v1/bots/1147792/new-webhook?&command=onWebhook&public_user_token=4a7753cc11d895775af563e429632745&user_id=20228307"; // Replace with your webhook URL
+
+  let name = document.getElementById("name").value;
+  let number = document.getElementById("number").value;
+  let message = document.getElementById("message").value;
+  
+  let requestData = {
+    "name": name, "number": number, "msg": message 
+  };
+
+  $.ajax({
+    type: "POST",
+    url: webhookUrl,
+    data: JSON.stringify(requestData),
+    success: function() {
+      alert("Message sent successfully");
+      location.reload(); // Reload the page after successful webhook request
+    },
+    error: function(xhr, status, error) {
+      console.error('Error sending webhook request:', error);
+      // Display error message to the user
+    },
+    dataType: "json"
+  });
+});
