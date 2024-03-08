67e3262942c421f929d076c49356e12e3e09b224
Rajdip <108176929+Rajdip585@users.noreply.github.com>
2024-03-08 19:31:35 +0530

    update webhook link

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.onscroll = () => {
 
 document.getElementById("contactForm").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent default form submission
-  var webhookUrl = "https://api.bots.business/v1/bots/1147792/new-webhook?&command=onMsgIncome&public_user_token=0026d52035fc95b469d72bcd43a824c9&user_id=20228307"; // Replace with your webhook URL
+  var webhookUrl = "https://api.bots.business/v1/bots/1147792/new-webhook?&command=OnRequest&public_user_token=0c19678ef50f732be57484151c307f00&user_id=20228307"; // Replace with your webhook URL
 
   let name = document.getElementById("name").value;
   let number = document.getElementById("number").value;
